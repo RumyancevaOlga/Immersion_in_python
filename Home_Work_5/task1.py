c90325bf4ffdda82393803d63cf34a378e496396
Rumyanceva <nj89.89@mail.ru>
2023-06-25 18:29:22 +0300

    Add changes in HW5

--- Home_Work_5/task1.py
+++ Home_Work_5/task1.py
@@ -3,12 +3,11 @@
 # расширение файла.
 
 def path_name_extension(path: str) -> tuple:
-    *path, name = path.split('/')
-    path = '/'.join(path)
+    path, name = path.rsplit('\\', 1)
     name, extension = name.split('.')
     res = [path, name, extension]
     return tuple(res)
 
 
-path_to_file = 'G:/Домашнее задание GeekBrains/Immersion_in_python/Home_Work_5/task1.py'
+path_to_file = r'G:\Домашнее задание GeekBrains\Immersion_in_python\Home_Work_5\task1.py'
 print(path_name_extension(path_to_file))
