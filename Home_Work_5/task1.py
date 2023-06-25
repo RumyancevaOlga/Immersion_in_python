719e752b98440185059b2874d6cbed97777cf38e
Rumyanceva <nj89.89@mail.ru>
2023-06-25 18:30:49 +0300

    Add more changes in HW5

--- Home_Work_5/task1.py
+++ Home_Work_5/task1.py
@@ -5,8 +5,7 @@
 def path_name_extension(path: str) -> tuple:
     path, name = path.rsplit('\\', 1)
     name, extension = name.split('.')
-    res = [path, name, extension]
-    return tuple(res)
+    return path, name, extension
 
 
 path_to_file = r'G:\Домашнее задание GeekBrains\Immersion_in_python\Home_Work_5\task1.py'
