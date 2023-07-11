f73ee59387a7d4bb6418d1516bc2634fa246846a
Rumyanceva <nj89.89@mail.ru>
2023-07-11 19:23:08 +0300

    Add changes

--- Home_Work_10/task1.py
+++ Home_Work_10/task1.py
@@ -9,24 +9,27 @@ from task5 import Birds, Fish, Mammals, Animals
 class Fabric:
     def __init__(self, obj_class, *args, **kwargs):
         self.obj_class = obj_class
-        if self.obj_class == 'Birds':
+        if self.obj_class == Birds:
             self.object = Birds(*args, **kwargs)
-        elif self.obj_class == 'Fish':
+        elif self.obj_class == Fish:
             self.object = Fish(*args, **kwargs)
-        elif self.obj_class == 'Mammals':
+        elif self.obj_class == Mammals:
             self.object = Mammals(*args, **kwargs)
         else:
             self.object = Animals(*args, **kwargs)
 
+    def get_object(self):
+        return self.object
 
-dog = Fabric('Mammals', False, 'Собака', True)
-print(f'Название: {dog.object.name}, хвост: {dog.object.tail}, спячка: {dog.object.specific()}')
 
-swan = Fabric('Birds', 4, 'Лебедь', True)
-print(f'Название: {swan.object.name}, хвост: {swan.object.tail}, длина крыла: {swan.object.specific()}')
+dog = Fabric(Mammals, False, 'Собака', True).get_object()
+print(f'Название: {dog.name}, хвост: {dog.tail}, спячка: {dog.specific()}')
 
-shark = Fabric('Fish', 15, 'Акула', True)
-print(f'Название: {shark.object.name}, хвост: {shark.object.tail}, глубина обитания: {shark.object.specific()}')
+swan = Fabric(Birds, 4, 'Лебедь', True).get_object()
+print(f'Название: {swan.name}, хвост: {swan.tail}, длина крыла: {swan.specific()}')
 
-viper = Fabric('Reptile', 'Гадюка', True)
-print(f'Название: {viper.object.name}, хвост: {viper.object.tail}')
+shark = Fabric(Fish, 15, 'Акула', True).get_object()
+print(f'Название: {shark.name}, хвост: {shark.tail}, глубина обитания: {shark.specific()}')
+
+viper = Fabric(Animals, 'Гадюка', True).get_object()
+print(f'Название: {viper.name}, хвост: {viper.tail}')
